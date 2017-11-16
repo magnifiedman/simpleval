0efdc10f16da24be1f2a3e4c2ba9f85e96800e08
Travis Wachendorf <travis@magnifiedonline.com>
2017-11-16 14:54:38 -0700

    First commit

---
+++ README.md
@@ -0,0 +1,47 @@
+# Simple Val Form Validation
+
+I simple jQuery include for validating your forms before submit.
+
+## Getting Started
+
+Just link to the simpleval.js file from your page with your form. Creating required fields is a breeze...
+
+### Installing and Using
+
+```
+Include the simpleval.js file at the end of your <body> tag after you include your desired jQuery library file
+<script src="PATH_TO_FOLDER/simpleval.js"</script>
+```
+In your HTML form code, simply add the class="required" to any field that you wish to require. Validation will be done on all fields simultaneously.
+```
+Form is tied to jQuery .submit(), so if you wish to tie any ajax to submission, use that hook.
+```
+
+
+## Running the tests
+
+Explain how to run the automated tests for this system
+
+### Break down into end to end tests
+
+Explain what these tests test and why
+
+```
+Give an example
+```
+
+### And coding style tests
+
+Explain what these tests test and why
+
+```
+Give an example
+```
+
+## Authors
+
+* **Travis Wachendorf** - [Midwestern Analog](https://github.com/magnifiedman)
+
+## License
+
+This project is licensed under the GNU General Public License
---
+++ js/simpleval.js
@@ -0,0 +1,273 @@
+$( document ).ready(function() {
+
+
+
+
+	// INIT AND CONFIG
+	
+
+	$('input[type="tel"]').attr('data-value','');
+	$('input[type="tel"]').attr('placeholder','XXX-XXX-XXXX');
+	$('.ccnum').attr('data-value','');
+	$('.ccnum').attr('placeholder','XXXX-XXXX-XXXX');
+	$('.ccexp').attr('data-value','');
+	$('.ccexp').attr('placeholder','XX/XXXX');
+	$('.money').attr('data-value','');
+	$('.money').attr('placeholder','$0.00');
+
+
+
+
+	// BASIC FUNCTIONS
+
+
+	// limit characters in field
+	function limitText(field, maxChar){
+		var ref = $(field),
+		val = ref.val();
+		if ( val.length >= maxChar ){
+			ref.val(function() {
+				console.log(val.substr(0, maxChar))
+				return val.substr(0, maxChar);       
+			});
+		}
+	}
+
+
+	// email validation
+	function validateEmail( email ) {
+		var emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+		return emailReg.test( email );
+	}
+
+
+	
+
+	// FORMATTING DATA FIELDS
+	 
+	
+	// telephone number [type="tel"]
+	$('input[type="tel"]').on( "keyup", function( e ) {
+
+		// only allow numbers and -
+		this.value = this.value.replace(/[^0-9\-]/g,'');
+
+		limitText(this,12);
+		var val = this.value.replace(/\D/g, '');
+		var curLength = val.length;
+
+		var newVal = '';
+		for( i=0; i<2; i++ ){
+			if (val.length > 3 && i<7) {
+				newVal += val.substr(0, 3) + '-';
+				val = val.substr(3);
+			}
+			else {
+				newVal += val.substr(0, 3);
+				val = val.substr(3);
+			}
+		}
+		
+		newVal += val;
+
+		actualVal  = newVal.replace(/\D/g, '');
+		$(this).attr('data-value',actualVal);
+
+		this.value = newVal;
+
+	});
+
+
+	// credit card number [class="ccnum"]
+	$('.ccnum').on( "keyup", function( e ) {
+
+		// only allow numbers and -
+		this.value = this.value.replace(/[^0-9\-]/g,'');
+
+		limitText(this,19);
+		var fVal = this.value;
+		var val = this.value.replace(/\D/g, '');
+		var curLength = val.length;
+		
+		var newVal = '';
+		for( i=0; i<4; i++){
+			if (val.length > 4 && i<20) {
+				newVal += val.substr(0, 4) + '-';
+				val = val.substr(4);
+			}
+			else {
+				newVal += val.substr(0, 4);
+				val = val.substr(4);
+			}
+		}
+
+		newVal += val;
+
+		actualVal  = newVal.replace(/\D/g, '');
+		$(this).attr('data-value',actualVal);
+		
+		this.value = newVal;
+
+	});
+
+
+	// credit card expiration date [class="ccexp"]
+	$('.ccexp').on( "keyup", function( e ) {
+
+		// only allow numbers and /
+		this.value = this.value.replace(/[^0-9\/]/g,'');
+
+		limitText(this,7);
+
+		var fVal = this.value;
+		var aVal= this.value.replace(/\D/g, '');
+		var curLength = aVal.length;
+		
+		$(this).attr('data-value',aVal);
+		
+		var newVal = '';
+			
+		if ( curLength == 2 ) {
+			newVal += aVal.substr(0, 2) + '/';
+		}
+		else {
+			newVal = fVal;
+		}
+
+		actualVal  = newVal.replace(/\D/g, '');
+		$(this).attr('data-value',actualVal);
+
+		this.value = newVal;
+
+	});
+
+
+	// money [class="money"]
+	$('.money').on( "keyup", function( e ) {
+
+		// only allow numbers and -
+		this.value = this.value.replace(/[^0-9\.\$]/g,'');
+
+		var fVal = this.value;
+		var aVal= this.value.replace(/\D/g, '');
+		var curLength = aVal.length;
+		
+		var newVal = '';
+			
+		if ( curLength <= 2 ) {
+			newVal += '$ .' + aVal;
+		}
+		else {
+			sliceChars = curLength-2;
+			dolAmount = aVal.substr(0,sliceChars);
+			decAmount = aVal.substr(-2);
+			newVal = '$ ' + dolAmount + '.' + decAmount;
+		}
+
+		actualVal  = newVal.replace("$ ", '');
+		$(this).attr('data-value',actualVal);
+
+		this.value = newVal;
+
+	});
+
+
+
+
+	// FORM VALIDATION
+	$('.forms input[type="submit"]').click( function( e ) {		
+
+		// prevent default
+		e.preventDefault();
+
+
+		// identify the current form
+		var theForm = $(this).closest('form');
+
+
+
+		// set validation vars
+		var formError=false;
+		var formFields = theForm.find(':input');
+		var required = theForm.find('.required');
+		var email = theForm.find('.email');
+		var messageBox = theForm.find('.message');
+		var passwordMatch = true
+		
+
+		// clear out prior error classes
+		required.removeClass('form-error');
+
+
+		// transfer data values from formatted fields
+		formFields.each(function(index,item){   
+			 if( $(item).attr('data-value')>0 ){
+			 	newVal = $(item).attr('data-value');
+			 	$(item).val(newVal);
+			 }
+		});
+
+
+		// validate required fields
+		required.each(function(index,item){   
+
+		   // field is empty - (text, textarea, email, select)
+		    if( $(item).val().length === 0 ){
+		        $(item).addClass('form-error');
+		        $(item).attr("placeholder", "This field is required.");
+		        formError=true;
+		    }
+
+		    // checkbox is not checked
+		    if($(item).attr('type')=='checkbox' && $(item).is(':checked')==false ){
+		    	formError=true;
+		    	$(item).addClass('form-error');
+		    }
+
+		    // email field incorrectly formatted
+		    if($(item).val().length !=0 && $(item).attr('type')=='email'){
+		    	var emailAddress =  $(item).val();
+			if( !validateEmail( emailAddress ) ){
+				formError=true;
+				$(item).val('');
+				$(item).attr("placeholder", "Please enter a valid email address");
+				$(item).addClass('form-error');
+			}
+		    }
+
+		});
+
+
+		// radio buttons only check that there is selected option
+		$("input:radio").each(function(){
+			var name = $(this).attr("name");
+			if($("input:radio[name="+name+"]:checked").length == 0){
+				formError=true;
+				$(this).addClass('form-error');
+			}
+		});
+
+
+		// password confirmation
+		if( $('.password').val() !== $('.password-match').val() ){
+			formError=true;
+			passwordMatch = false;
+		}
+
+
+		// final processing
+		if( formError===true ){
+			messageBox.addClass('error');
+			messageBox.html('You have left some required fields blank.');
+			if( passwordMatch==false ){ messageBox.append('* Passwords must match.'); }
+			messageBox.show();
+			$("html, body").delay(2000).animate({
+			        scrollTop: messageBox.offset().top 
+			}, 2000);
+		} else {
+			theForm.submit();
+		}
+
+	});
+
+});
